1f89fe4d66b2baad8e992daaea1fd79b2cae56c9
fitsum yoseph <fitsumyoseph1@gmail.com>
2025-05-18 23:15:24 +0300

    deltabackend deleted

--- delta_backend-main/startup/config.js
+++
@@ -1,7 +0,0 @@
-const config = require("config");
-module.exports = function () {
-  if (!config.get("jwtPrivateKey")) {
-    console.error("FATAL ERROR: JWT Private Key is not defined");
-    process.exit(1); // exit the application with an error status code 1
-  }
-};
--- delta_backend-main/startup/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-
-const { logger } = require("../middlewares/error");
-module.exports=function(){
-    mongoose.connect('mongodb://localhost:27017/delta')
-    .then(()=>logger.info('Connected to the database...'))
-}
